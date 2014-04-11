7ecf14bd1615f6e1873fefe33ea2647035d5dcb5
Carlos Gochez <cgotcha@gmail.com>
2014-04-11 08:33:13 -0600

    Hice la clase con el ejemplo de los primos

---
+++ src/repaso/Primo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package repaso;
+
+/**
+ *
+ * @author Docente 17082011
+ */
+public class Primo {
+    public static void main(String[] args) {
+        int num = 7;
+        int div = 0;
+        
+        for(int x=0; x <= num; x++){
+            if( div % num == 0)
+                div++;
+        }
+        
+        if(div == 2)
+            System.out.println("Es Primo");
+        else
+            System.out.println("No Es Primo");
+    }
+}
