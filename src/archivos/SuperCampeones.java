6dc3c88241b53ee05cb5cb83f5f8e0bbd702149b
Carlos Gochez <cgotcha@gmail.com>
2014-06-05 08:09:58 -0600

    Pauta prueba4 y seguir con team

--- src/archivos/SuperCampeones.java
+++ src/archivos/SuperCampeones.java
@@ -6,6 +6,10 @@
 
 package archivos;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Docente 17082011
@@ -13,5 +17,40 @@ package archivos;
 public class SuperCampeones {
     public static void main(String[] args) {
         TeamManagement tmn = new TeamManagement();
+        int op=0;
+        Scanner lea = new Scanner(System.in);
+        
+        do{
+            System.out.println("1- Adicionar Equipo");
+            System.out.println("2- Listar Equipos");
+            System.out.println("3- Agregar Jugadores a Temporada");
+            System.out.println("4- Adicionar goles a Jugador");
+            System.out.println("9- Salir");
+            System.out.println("Escoja opcion: ");
+            
+            try{
+                op = lea.nextInt();
+                
+                switch(op){
+                    case 1:
+                        System.out.println("Nombre: ");
+                        String team = lea.next();
+                        System.out.println("dt: ");
+                        String dt = lea.next();
+                        tmn.addEquipo(team, dt);
+                        break;
+                    case 2:
+                        tmn.printTeams();
+                        break;
+                }
+                
+            }
+            catch(InputMismatchException e){
+                lea.next();
+            }
+            catch(IOException e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }while(op!=9);
     }
 }
--- src/archivos/TeamManagement.java
+++ src/archivos/TeamManagement.java
@@ -2,14 +2,35 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
+    FORMATO de equipos.tmn
+    ---------------------------
+    int codigo
+    String nombre
+    long fundacion
+    String DT
+    boolean esprimera
+    int temporada actual
+
+    FORMATO de temporada#.tmn
+    -----------------------------
+    int año inicio
+    int año final
+    int numero camisa
+    String nombre
+    int goles
+    int asistencias
+    int minutos
+    double salario
+
  */
 
 package archivos;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -82,8 +103,54 @@ public class TeamManagement {
         
         createSeasonFile(tfolder,1);
     }
+    
+    public void printTeams()throws IOException{
+        rTeam.seek(0);
+        
+        while(rTeam.getFilePointer() < rTeam.length()){
+            int ce = rTeam.readInt();
+            String team = rTeam.readUTF();
+            Date fundacion = new Date(rTeam.readLong());
+            String DT = rTeam.readUTF();
+            boolean primera = rTeam.readBoolean();
+            rTeam.readInt();
+            
+            System.out.printf("%d-%s Fundado en %s, DT: %s - %s\n",
+                    ce,team,fundacion.toString(),DT,
+                    primera ? "Primera" : "Segunda");
+        }
+    }
 
     private void createSeasonFile(String teamFolder,int nt) throws IOException{
-        
+        String path = teamFolder+"/temporada"+nt+".tmn";
+        RandomAccessFile rSeason = new RandomAccessFile(path,"rw");
+        Calendar c = Calendar.getInstance();
+        int aa = c.get(Calendar.YEAR);
+        c.add(Calendar.YEAR, 1);
+        int af = c.get(Calendar.YEAR);
+        rSeason.writeInt(aa);
+        rSeason.writeInt(af);
+        rSeason.close();
+        System.out.printf("Creando temporada %d/%d....\n\n",aa,af);
     }
+    
+    private void addGoalsToPlayer(String teamFilePath,int nc,int gols)throws IOException{
+        RandomAccessFile rSeason = new RandomAccessFile(teamFilePath,"rw");
+        rSeason.skipBytes(8);
+        while(rSeason.getFilePointer() < rSeason.length()){
+            int numero = rSeason.readInt();
+            rSeason.readUTF();
+            long pos = rSeason.getFilePointer();
+            int goles = rSeason.readInt();
+            rSeason.skipBytes(16);
+            
+            if(numero == nc){
+                rSeason.seek(pos);
+                rSeason.writeInt(goles+gols);
+                break;
+            }
+        }
+        rSeason.close();
+    }
+    
 }
